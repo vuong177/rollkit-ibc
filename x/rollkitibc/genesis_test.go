d9f82340382283e5b81165b740fdd161cd05d072
Developer Experience team at Ignite <hello@ignite.com>
2023-11-24 21:18:09 +0700

    Initialized with Ignite CLI

---
+++ x/rollkitibc/genesis_test.go
@@ -0,0 +1,29 @@
+package rollkitibc_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	keepertest "rollkit-ibc/testutil/keeper"
+	"rollkit-ibc/testutil/nullify"
+	"rollkit-ibc/x/rollkitibc"
+	"rollkit-ibc/x/rollkitibc/types"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.RollkitibcKeeper(t)
+	rollkitibc.InitGenesis(ctx, *k, genesisState)
+	got := rollkitibc.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
